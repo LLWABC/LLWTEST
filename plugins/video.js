baf8f751635f8030f5700cb5d8519ec45119655b
LLWABC <shewagama@gmail.com>
2025-03-26 21:26:35 +0530

    Update video.js

--- plugins/video.js
+++ plugins/video.js
@@ -1,6 +1,6 @@
 const { cmd, commands } = require('../command');
 const yts = require('yt-search');
-const ytmp4 = require('ytmp4'); // Importing the denethdev-ytmp3 package for downloading
+const ddownr = require('ytmp4'); // Importing the denethdev-ytmp3 package for downloading
 
 cmd({
   pattern: "video",
@@ -22,7 +22,7 @@ cmd({
     const videoUrl = videoData.url;
 
     // Using denethdev-ytmp3 to fetch the download link
-    const result = await ytmp4.download(videoUrl, 'mp4'); // Download in mp3 format
+    const result = await ddownr.download(videoUrl, 'mp4'); // Download in mp3 format
     const downloadLink = result.downloadUrl; // Get the download URL
 
     let videoDetailsMessage = `〽️ *LLW MD V1 VIDEO DOWNLOADER* 〽️\n\n`;
